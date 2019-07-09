4d0e45ef59ea0cfafcb793d16f95dbc35c74e74c
Stefan kruger <stefan.kruger@gmail.com>
2019-07-09 11:51:29 +0100

    day12 p2

--- day12.rkt
+++ day12.rkt
@@ -9,6 +9,7 @@
 ;; Stefan Kruger
 
 (require math)  ; sum
+(require json)
 
 (define (read-data [filename "data/input12.data"])
   (car (file->lines filename)))
@@ -16,8 +17,24 @@
 (define (all-numbers str)
   (map string->number (regexp-match* #px"-?\\d+" str)))
 
+(define (parse-json str)
+  (with-input-from-string str (λ () (read-json))))
+
+(define (hash-has-value? haystack needle)
+  (set-member? (list->set (hash-values haystack)) needle))
+
+(define (remove-red json-data [total 0])
+  (cond [(number? json-data) (+ total json-data)]
+        [(and (hash? json-data) (not (hash-has-value? json-data "red")))
+         (+ total (sum (for/list ([v (in-hash-values json-data)]) (remove-red v total))))]
+        [(list? json-data)
+         (+ total (sum (for/list ([v (in-list json-data)]) (remove-red v total))))]
+        [else total]))
+
 (define (main)
-  (let ([data-str (read-data)])
-    (printf "Part1: ~a\n" (sum (all-numbers data-str)))))
+  (let* ([data-str (read-data)]
+         [json-data (parse-json data-str)])
+    (printf "Part1: ~a\n" (sum (all-numbers data-str)))
+    (printf "Part2: ~a\n" (remove-red json-data))))
 
 (main)
