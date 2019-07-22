2ce6bd610381422ab8079a5d626fd56d3d9caa1d
Stefan kruger <stefan.kruger@gmail.com>
2019-07-22 15:37:39 +0100

    wip

--- day19.rkt
+++ day19.rkt
@@ -8,8 +8,6 @@
 ;;
 ;; Stefan Kruger
 
-(require math)
-
 (define (read-data [filename "data/input19.data"])
   (file->lines filename))
 
@@ -21,21 +19,40 @@
         ['() (loop rules rst)]
         [(list target "=>" replacement) (loop (cons (cons target replacement) rules) rst)]))))
     
-(define (apply-rule molecule rule)
-  (let loop ([prefix ""] [results '()] [matches (string-split molecule (car rule) #:trim? #f)])
+(define (apply-rule molecule key replacement)
+  (let loop ([prefix ""] [results '()] [matches (string-split molecule key #:trim? #f)])
     (match matches
       [(list _) results]
       [_ (match-let* ([(list this rest ...) matches]
-                      [tail-str (string-join rest (car rule))]
-                      [new-string (string-join (append `(,prefix ,this ,(cdr rule) ,tail-str)) "")]
-                      [new-prefix (string-join `(,prefix ,this ,(car rule)) "")])
+                      [tail-str (string-join rest key)]
+                      [new-string (string-join (append `(,prefix ,this ,replacement ,tail-str)) "")]
+                      [new-prefix (string-join `(,prefix ,this ,key) "")])
            (loop new-prefix (cons new-string results) rest))])))
 
 (define (unique-molecules data)
-  (set-count (list->set (append-map (curry apply-rule (cdr data)) (car data)))))
+  (set-count (list->set (append-map (λ (r) (apply-rule (cdr data) (car r) (cdr r))) (car data)))))
 
+;; Part 2: starting at "e" what's the lowest number of transformations required to get
+;; to the target molecule? We can solve this by going backwards: at each state, only
+;; rules whose replacement is present in the molecule could have been applied.
+(define (step-count rules molecule)
+  (let loop ([target molecule] [count 0])
+    (cond [(equal? target "e") count]
+          [else
+           (printf "target: ~a -> " target)
+           (let ([count&target
+                  (for/fold ([c 0] [s target] #:result (cons c s))
+                            ([r (in-list rules)] #:when (string-contains? target (cdr r)))
+                    (values (add1 c) (string-replace s (cdr r) (car r) #:all? #f)))])
+             (printf "~a\n" (cdr count&target))
+             (cond [(equal? target (cdr count&target))
+                    (step-count (shuffle rules) molecule)] ;; dead end. Shuffle rule-order and re-try
+                   [else
+                    (loop  (cdr count&target) (+ count (car count&target)))]))])))
+                 
 (define (main)
-  (let* ([data (parse-data (read-data))])
-    (printf "Part1: ~a\n" (unique-molecules data))))
+  (let ([data (parse-data (read-data "data/test.data"))])
+    (printf "Part1: ~a\n" (unique-molecules data))
+    (printf "Part2: ~a\n" (step-count (car data) (cdr data)))))
     
-(main)
+;(main)
