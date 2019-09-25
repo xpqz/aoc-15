dd4126ea444f4d2e9f62f7ef6f0232a922af63e0
Stefan kruger <stefan.kruger@gmail.com>
2019-09-25 14:35:11 +0100

    now with macro

--- day22.rkt
+++ day22.rkt
@@ -3,70 +3,45 @@
 (require racket/match)
 (require data/queue)
 
-(provide (all-defined-out))
-
-(struct player (hp ap mana shield poison recharge) #:transparent)
+(struct player (hp ap mana [shield #:mutable] [poison #:mutable] [recharge #:mutable]) #:transparent)
 (struct boss (hp dp) #:transparent)
-(struct state (us them spell spend) #:transparent)
-
-(define MM-COST 53)
-(define DR-COST 73)
-(define SH-COST 113)
-(define PO-COST 173)
-(define RE-COST 229)
+(struct state (us them spend) #:transparent)
 
 (define (finished? us them deficit)
   (or (<= (- (player-hp us) deficit) 0) (<= (boss-hp them) 0)))
-  
-(define (magic-missile old-state deficit)
-  ;; If we can afford it, cast magic missile
-  (match-let ([(state us them _ spend) old-state])
-    (cond [(finished? us them deficit) '()]
-          [(>= (player-mana us) MM-COST)
-           (define np (struct-copy player us [mana (- (player-mana us) MM-COST)] [hp (- (player-hp us) deficit)]))
-           (define nb (struct-copy boss them [hp (- (boss-hp them) 4)]))
-           (state np nb 'magicmissile (+ spend MM-COST))]
-          [else '()])))
 
-(define (drain old-state deficit)
-  ;; If we can afford it, cast drain
-  (match-let ([(state us them _ spend) old-state])
-    (cond [(finished? us them deficit) '()]
-          [(>= (player-mana us) DR-COST)
-           (define np (struct-copy player us [mana (- (player-mana us) DR-COST)] [hp (- (+ (player-hp us) 2) deficit)]))
-           (define nb (struct-copy boss them [hp (- (boss-hp them) 2)]))
-           (state np nb 'drain (+ spend DR-COST))]
-          [else '()])))
+(define (active-effect? us sh po re)
+  (or (and (> sh 0) (> (player-shield us) 0))
+      (and (> po 0) (> (player-poison us) 0))
+      (and (> re 0) (> (player-recharge us) 0))))
 
-(define (shield old-state deficit)
-  ;; If we can afford it, and we don't already have one running, cast shield
-  (match-let ([(state us them _ spend) old-state])
-    (cond [(finished? us them deficit) '()]
-          [(and (<= (player-shield us) 0) (>= (player-mana us) SH-COST))
-           (define np (struct-copy player us [mana (- (player-mana us) SH-COST)] [shield 6] [hp (- (player-hp us) deficit)]))
-           (state np them 'shield (+ spend SH-COST))]
-          [else '()])))
-
-(define (poison old-state deficit)
-  ;; If we can afford it, and we don't already have one running, cast poison
-  (match-let ([(state us them _ spend) old-state])
-    (cond [(finished? us them deficit) '()]
-          [(and (<= (player-poison us) 0) (>= (player-mana us) PO-COST))
-           (define np (struct-copy player us [mana (- (player-mana us) PO-COST)] [poison 6] [hp (- (player-hp us) deficit)]))
-           (state np them 'poison (+ spend PO-COST))]
-          [else '()])))
-
-(define (recharge old-state deficit)
-  ;; If we can afford it, and we don't already have one running, cast recharge
-  (match-let ([(state us them _ spend) old-state])
-    (cond [(finished? us them deficit) '()]
-          [(and (<= (player-recharge us) 0) (>= (player-mana us) RE-COST))
-           (define np (struct-copy player us [mana (- (player-mana us) RE-COST)] [recharge 5] [hp (- (player-hp us) deficit)]))
-           (state np them 'recharge (+ spend RE-COST))]
-          [else '()])))
+(define-syntax (spell stx)
+  (syntax-case stx ()
+    [(_ name cost damage armor hitp sh po re)
+     #'(define (name old-state [deficit 0])
+         (match-let ([(state us them spend) old-state])
+           (cond [(finished? us them deficit) '()]
+                 [(active-effect? us sh po re) '()]
+                 [(< (player-mana us) cost) '()]
+                 [else
+                  (define np
+                    (struct-copy player us [mana (- (player-mana us) cost)]
+                                 [hp (+ (- (player-hp us) deficit) hitp)]
+                                 [ap (+ (player-ap us) armor)]))
+                  (when (> sh 0) (set-player-shield! np sh))
+                  (when (> po 0) (set-player-poison! np po))
+                  (when (> re 0) (set-player-recharge! np re))
+                  (define nb (struct-copy boss them [hp (- (boss-hp them) damage)]))
+                  (state np nb (+ spend cost))])))]))
+                           
+(spell magic-missile 53 4 0 0 0 0 0)
+(spell drain 73 2 0 2 0 0 0)
+(spell shield 113 0 0 0 6 0 0)
+(spell poison 173 0 0 0 0 6 0)
+(spell recharge 229 0 0 0 0 0 5)
 
 (define (apply-effects old-state)
-  (match-let ([(state us them _ _) old-state])
+  (match-let ([(state us them _) old-state])
     (define (dec0 v) (if (> v 0) (sub1 v) 0))
     (define damage-given (if (> (player-poison us) 0) 3 0))
     (define armour (if (> (player-shield us) 0) 7 0))
@@ -77,13 +52,11 @@
                             [shield (dec0 (player-shield us))]
                             [poison (dec0 (player-poison us))]
                             [recharge (dec0 (player-recharge us))]))
-    (struct-copy state
-                 old-state
-                 [us np]
+    (struct-copy state old-state [us np]
                  [them (struct-copy boss them [hp (- (boss-hp them) damage-given)])])))
   
 (define (boss-turn old-state)
-  (match-let ([(state us them _ _) old-state])
+  (match-let ([(state us them _) old-state])
     (define damage (let ([d (- (boss-dp them) (player-ap us))]) (if (<= d 0) 1 d)))
     (struct-copy state old-state [us (struct-copy player us [hp (- (player-hp us) damage)])])))
                           
@@ -95,11 +68,10 @@
                 (poison old-state deficit)
                 (recharge old-state deficit))))
   
-(define (state-transition old-state deficit)
+(define (state-transition old-state [deficit 0])
   (map (compose1 apply-effects boss-turn apply-effects) (player-turn old-state deficit)))
 
 (define (find-cheapest-win start [deficit 0])
-  ;; Breadth-first search for lowest-cost win. 
   (define (winner? s)
     (<= (boss-hp (state-them s)) 0))
   (define Q (make-queue))
@@ -117,14 +89,12 @@
                 (set-add! visited neighbour)
                 (when (and (winner? neighbour) (< (state-spend neighbour) (state-spend winner)))
                   (set! winner neighbour))))
-                (state-transition current deficit))
-              (loop)])))
-
+            (state-transition current deficit))
+           (loop)])))
                 
 (define (main)
-  (define start-state (state (player 50 0 500 0 0 0) (boss 71 10) 'start 0))
-  (printf "Part1: ~a\n" (state-spend (find-cheapest-win start-state 0)))
+  (define start-state (state (player 50 0 500 0 0 0) (boss 71 10) 0))
+  (printf "Part1: ~a\n" (state-spend (find-cheapest-win start-state)))
   (printf "Part2: ~a\n" (state-spend (find-cheapest-win start-state 1))))
 
 (main)
-  
